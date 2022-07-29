8454cd0b565c04c396375b6ecd37cbf710597dcb
Uros Relic <urosrelicc@gmail.com>
2022-07-29 16:54:29 +0200

    add change grid size and drawing functionality

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const grid = document.querySelector('.grid');
 const slider = document.querySelector('#range-slider');
 const sliderSpan = document.querySelector("#slider-value");
+const color = document.querySelector('#color-picker');
 
 function populateGrid(size) {
     grid.style.setProperty('--size', size);
     for(let i = 0; i < size * size; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
+
+        div.addEventListener('mousemove', function(){
+            div.style.backgroundColor = color.value
+        })
         grid.appendChild(div);
     }
 }
 
 function updateSliderValue() {
-    sliderSpan.innerHTML = "Value " + slider.value;
+    sliderSpan.innerHTML = slider.value + "x" + slider.value;
+}
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
+function resetGrid(size) {
+    clearGrid();
+    populateGrid(size);
 }
 
 slider.addEventListener('input', updateSliderValue);
-
-
-populateGrid(8);
+slider.onchange = (e) => resetGrid(e.target.value);
+window.onload = () => populateGrid(16);

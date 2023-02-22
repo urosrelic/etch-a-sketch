6648e666b65ec023590a7121ebbc3bb801f7c17b
urosrelic <urosrelicc@gmail.com>
2023-02-22 09:06:37 +0100

    add coloring functionality

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 const grid = document.querySelector(".grid");
 const sizeSlider = document.querySelector("#size-slider");
+const colorPicker = document.querySelector("#color-picker");
 const sliderValue = document.querySelector(".sliderValue");
+const colorBtn = document.querySelector("#color-btn");
+const rainbowBtn = document.querySelector("#rainbow-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+const defaultColor = "#000"
 const defaultSize = 16;
+const defaultMode = "color"; // Available modes - Color, Rainbow, Eraser, Clear
 
 let currentSize = defaultSize;
+let currentMode = defaultMode;
+let currentColor = defaultColor;
+
+let mouseDown = false
+grid.onmousedown = () => (mouseDown = true)
+grid.onmouseup = () => (mouseDown = false)
 
 function setNewSize(size) {
     currentSize = size;
+}
+
+function setNewMode(mode) {
+    setToggle(mode);
+    currentMode = mode;
+}
+
+function setNewColor(color) {
+    currentColor = color;
+}
+
+function setToggle(mode) {
+    if(mode === "color") {
+        colorBtn.classList.add("active");
+        rainbowBtn.classList.remove("active");
+        eraserBtn.classList.remove("active");
+    } else if(mode === "rainbow") {
+        colorBtn.classList.remove("active");
+        rainbowBtn.classList.add("active");
+        eraserBtn.classList.remove("active");
+    } else if(mode === "eraser") {
+        colorBtn.classList.remove("active");
+        rainbowBtn.classList.remove("active");
+        eraserBtn.classList.add("active");
+    } else {
+        colorBtn.classList.remove("active");
+        rainbowBtn.classList.remove("active");
+        eraserBtn.classList.remove("active");
+    }
 }
 
 function populateGrid(size) {
@@ -15,7 +58,31 @@ function populateGrid(size) {
     for(let i = 0; i < size * size; i++) {
         let box = document.createElement("div");
         box.classList.add("box");
+        // add event listeners to each box
+        box.addEventListener("mouseover", changeColor);
+        box.addEventListener("mousedown", changeColor);
+
         grid.appendChild(box);
+    }
+}
+
+function changeColor(e) {
+    if(!mouseDown) {
+        return;
+    }
+    switch(currentMode) {
+        case "color":
+            e.target.style.backgroundColor = currentColor;
+            break;
+        case "rainbow":
+            e.target.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+            break;
+        case "eraser":
+            e.target.style.backgroundColor = "#fff";
+            break;
+        default:
+            e.target.style.backgroundColor = currentColor;
+            break;
     }
 }
 
@@ -38,9 +105,13 @@ function changeSize(size) {
     reloadGrid();
 }
 
-
+colorPicker.onchange = (e) => setNewColor(e.target.value);
 sizeSlider.onmousemove = () => setSliderValue(sizeSlider.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
+colorBtn.onclick = () => setNewMode("color");
+rainbowBtn.onclick = () => setNewMode("rainbow");
+eraserBtn.onclick = () => setNewMode("eraser");
+clearBtn.onclick = () => reloadGrid();
 
 window.onload = () => {
     setSliderValue(defaultSize);
